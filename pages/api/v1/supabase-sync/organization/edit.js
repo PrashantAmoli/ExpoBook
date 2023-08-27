691b68c42a7484d86fb3d2e1e5752aaaefc6e306
prashantsuntel <111524783+prashantsuntel@users.noreply.github.com>
2023-08-27 23:33:00 +0530

    Upsert user and organization operation added

--- pages/api/v1/supabase-sync/organization/edit.js
+++ pages/api/v1/supabase-sync/organization/edit.js
@@ -5,21 +5,40 @@ export default async function handler(req, res) {
   try {
     switch (evt.type) {
       case "organization.updated":
-        await supabase
-          .from("organizations")
-          .update({
-            name: evt.data.name,
-            slug: evt.data.slug,
-            clerk_data: evt.data,
-            updated_at: new Date(),
-          })
-          .eq("id", evt.data.id)
-          .then((response) => {
-            res.status(200).json({ message: "Organization updated successfully!" });
-          })
-          .catch((e) => {
-            res.status(500).json({ message: "Update organization operation failed! " + e });
-          });
+        const organizations = await supabase.from("organizations").select().eq("id", evt.data.id);
+
+        if (organizations.data.length) {
+          await supabase
+            .from("organizations")
+            .update({
+              name: evt.data.name,
+              slug: evt.data.slug,
+              clerk_data: evt.data,
+              updated_at: new Date(),
+            })
+            .eq("id", evt.data.id)
+            .then((response) => {
+              res.status(200).json({ message: "Organization updated successfully!" });
+            })
+            .catch((e) => {
+              res.status(500).json({ message: "Update organization operation failed! " + e });
+            });
+        } else {
+          await supabase
+            .from("organizations")
+            .insert({
+              id: evt.data.id,
+              name: evt.data.name,
+              slug: evt.data.slug,
+              clerk_data: evt.data,
+            })
+            .then((response) => {
+              res.status(200).json({ message: "Organization added successfully!" });
+            })
+            .catch((e) => {
+              res.status(500).json({ message: "Add organization operation failed! " + e });
+            });
+        }
         break;
       default:
         res.status(501).json({ message: "Operation not supported!" });
